3242552a95d7389444f26f2972ac6100151caf01
Shantanu Kumar <rajatmaurya34@gmail.com>
2026-05-29 23:43:05 +0100

    modules

---
+++ src/geometry.rs
@@ -0,0 +1,10 @@
+pub struct Point2d {
+    pub x: u32,
+    pub y: u32,
+}
+
+pub struct Point3d {
+    pub x: u32,
+    pub y: u32,
+    pub z: u32,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,47 @@
 use minifb::{Window, WindowOptions};
+mod color;
+mod geometry;
+mod image;
+
+use color::Color;
+use geometry::{Point2d, Point3d};
+use image::Image;
 
 const W: u32 = 800;
 const H: u32 = 600;
 
-struct Point2d {
-    x: u32,
-    y: u32,
-}
-
-struct Point3d {
-    x: u32,
-    y: u32,
-    z: u32,
-}
-
-struct Color {
-    r: u8,
-    g: u8,
-    b: u8,
-    a: u8,
-}
-
-impl Color {
-    fn to_u32(&self) -> u32 {
-        ((self.r as u32) << 16) | ((self.g as u32) << 8) | (self.b as u32)
-    }
-}
-
-struct Image {
-    width: u32,
-    height: u32,
-    buffer: Vec<u32>,
-}
-
-impl Image {
-    fn new(width: u32, height: u32) -> Self {
-        Image {
-            width: width,
-            height: height,
-            buffer: vec![0u32; (width * height) as usize],
-        }
-    }
-
-    fn fill(&mut self, value: u32) {
-        self.buffer.fill(value);
-    }
-
-    fn update_px_color(&mut self, point: Point2d, color: &Color) {
-        let index = (point.y * self.width + point.x) as usize;
-        self.buffer[index] = color.to_u32();
-    }
-}
-
 fn create_line_on_image(image: &mut Image, point_1: Point2d, point_2: Point2d, color: &Color) {
-    let dx = point_2.x as i32 - point_1.x as i32;
-    let dy = point_2.y as i32 - point_1.y as i32;
-    let m = dy / dx;
-    let mut y = point_1.y as i32;
+    let mut x0 = point_1.x as i32;
+    let mut y0 = point_1.y as i32;
+    let x1 = point_2.x as i32;
+    let y1 = point_2.y as i32;
 
-    for x in point_1.x..point_2.x {
-        image.update_px_color(Point2d { x, y: y as u32 }, color);
-        y += m;
+    let dx = (x1 - x0).abs();
+    let dy = (y1 - y0).abs();
+    let sx = if x0 < x1 { 1 } else { -1 };
+    let sy = if y0 < y1 { 1 } else { -1 };
+    let mut err = dx - dy;
+
+    loop {
+        image.update_px_color(
+            Point2d {
+                x: x0 as u32,
+                y: y0 as u32,
+            },
+            color,
+        );
+        if x0 == x1 && y0 == y1 {
+            break;
+        }
+        let e2 = 2 * err;
+        if e2 > -dy {
+            err -= dy;
+            x0 += sx;
+        }
+        if e2 < dx {
+            err += dx;
+            y0 += sy;
+        }
     }
 }
 
