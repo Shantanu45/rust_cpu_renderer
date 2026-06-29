e801c60508a495f695f6f21f07bcb7bde1274970
Shantanu Kumar <skumar@climaxstudios.com>
2026-06-29 16:07:09 +0100

    mat3 math utils

--- src/games/asteroids.rs
+++ src/games/asteroids.rs
@@ -2,7 +2,7 @@ use std::os::raw::c_float;
 use crate::color::Color;
 use crate::game::{Game, GameCommand, GameContext};
 use crate::input::Input;
-use crate::math::Vec2i;
+use crate::math::{Vec2i, Mat3};
 use crate::renderer::Renderer;
 
 #[derive(Default)]
@@ -16,12 +16,18 @@ struct Ship {
 pub struct Asteroids
 {
     ship: Ship,
+    model_mat: Mat3,
 }
 
 impl Asteroids {
     pub fn new() -> Self{
         Self{
-            ship: Ship::default()
+            ship: Ship::default(),
+            model_mat: Mat3{
+                r1: [1.0, 0.0, 0.5],
+                r2: [0.0, -1.0, 0.5],
+                r3: [0.0, 0.0, 1.0],
+            }
         }
     }
 }
@@ -56,6 +62,5 @@ impl Asteroids{
         let flame_end = Vec2i{x: SHIP_MID_SECTION.x + (SHIP_RIGHT_WING.x - SHIP_MID_SECTION.x)/2, y: SHIP_MID_SECTION.y + 5 + (SHIP_RIGHT_WING.y - SHIP_MID_SECTION.y)/2};
         let flame_vert_offset_x = (flame_end.x - flame_start.x)/3;
         renderer.draw_line_connected(vec![flame_start, flame_start + Vec2i{x: flame_vert_offset_x, y: 5}, flame_start + Vec2i{x: flame_vert_offset_x*2, y: 5}, flame_end],false, Color::RED);
-
     }
 }
--- src/math.rs
+++ src/math.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+use std::fmt::Formatter;
 use std::ops::{Add, AddAssign, Mul, Sub, SubAssign};
 use std::ops::MulAssign;
 
@@ -74,6 +76,18 @@ impl SubAssign for Vec2i {
 }
 
 #[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Vec2 {
+    pub x: f32,
+    pub y: f32,
+}
+
+impl Vec2 {
+    pub const fn new(x: f32, y: f32) -> Self {
+        Self { x, y}
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Vec3 {
     pub x: f32,
     pub y: f32,
@@ -83,5 +97,128 @@ pub struct Vec3 {
 impl Vec3 {
     pub const fn new(x: f32, y: f32, z: f32) -> Self {
         Self { x, y, z }
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+pub struct Mat3 {
+    pub r1: [f32;3] ,
+    pub r2: [f32;3],
+    pub r3: [f32;3],
+}
+
+impl Mat3{
+    pub const fn new(r1: [f32;3], r2: [f32;3], r3: [f32;3]) -> Self{ Self{r1, r2, r3}}
+}
+
+impl Mul<Mat3> for Mat3{
+    type Output = Mat3;
+
+    fn mul(self, rhs: Mat3) -> Self::Output {
+        let r1 = [self.r1[0] * rhs.r1[0] + self.r1[1] * rhs.r2[0] + self.r1[2] * rhs.r3[0],
+                         self.r1[0] * rhs.r1[1] + self.r1[1] * rhs.r2[1] + self.r1[2] * rhs.r3[1],
+                         self.r1[0] * rhs.r1[2] + self.r1[1] * rhs.r2[2] + self.r1[2] * rhs.r3[2]];
+
+        let r2 = [self.r2[0] * rhs.r1[0] + self.r2[1] * rhs.r2[0] + self.r2[2] * rhs.r3[0],
+                         self.r2[0] * rhs.r1[1] + self.r2[1] * rhs.r2[1] + self.r2[2] * rhs.r3[1],
+                         self.r2[0] * rhs.r1[2] + self.r2[1] * rhs.r2[2] + self.r2[2] * rhs.r3[2]];
+
+        let r3 = [self.r3[0] * rhs.r1[0] + self.r3[1] * rhs.r2[0] + self.r3[2] * rhs.r3[0],
+                         self.r3[0] * rhs.r1[1] + self.r3[1] * rhs.r2[1] + self.r3[2] * rhs.r3[1],
+                         self.r3[0] * rhs.r1[2] + self.r3[1] * rhs.r2[2] + self.r3[2] * rhs.r3[2]];
+        Mat3{
+            r1,
+            r2,
+            r3
+        }
+    }
+}
+
+impl Mul<Vec3> for Mat3{
+    type Output = Vec3;
+
+    fn mul(self, rhs: Vec3) -> Self::Output {
+        Vec3{
+            x: self.r1[0] * rhs.x + self.r1[1] * rhs.y + self.r1[2] * rhs.z,
+            y: self.r2[0] * rhs.x + self.r2[1] * rhs.y + self.r2[2] * rhs.z,
+            z: self.r3[0] * rhs.x + self.r3[1] * rhs.y + self.r3[2] * rhs.z,
+        }
+    }
+}
+
+impl PartialEq for Mat3
+{
+    fn eq(&self, other: &Self) -> bool {
+        self.r1 == other.r1 && self.r2 == other.r2 && self.r3 == other.r3
+    }
+
+    fn ne(&self, other: &Self) -> bool {
+        !self.eq(other)
+    }
+}
+
+impl fmt::Display for Mat3
+{
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "[[\n{:.1}, {:.1}, {:.1}\n{:.1}, {:.1}, {:.1}\n{:.1}, {:.1}, {:.1}\n]]",
+               self.r1[0], self.r1[1], self.r1[2],
+               self.r2[0], self.r2[1], self.r2[2],
+               self.r3[0], self.r3[1], self.r3[2])
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_mat3_print(){
+        let m = Mat3{
+            r1: [1.0, 2.0, 3.0],
+            r2: [4.0, 5.0, 6.0],
+            r3: [7.0, 8.0, 9.0],
+        };
+        assert_eq!(format!("{}", m), "[[\n1.0, 2.0, 3.0\n4.0, 5.0, 6.0\n7.0, 8.0, 9.0\n]]");
+    }
+
+    #[test]
+    fn test_mat3_multiplication(){
+        let m1 = Mat3{
+            r1: [1.0, 2.0, 3.0],
+            r2: [4.0, 5.0, 6.0],
+            r3: [7.0, 8.0, 9.0],
+        };
+        let m2 = Mat3{
+            r1: [1.0, 2.0, 3.0],
+            r2: [4.0, 5.0, 6.0],
+            r3: [7.0, 8.0, 9.0],
+        };
+
+        let m = m1 * m2;
+        let m_ref = Mat3{
+            r1: [30.0, 36.0, 42.0],
+            r2: [66.0, 81.0, 96.0],
+            r3: [102.0, 126.0, 150.0]
+        };
+        assert_eq!(m, m_ref);
+    }
+    #[test]
+    fn test_mat3_vec_multiplication() {
+        let m = Mat3{
+            r1: [1.0, 2.0, 3.0],
+            r2: [4.0, 5.0, 6.0],
+            r3: [7.0, 8.0, 9.0],
+        };
+
+        let v = Vec3 {
+            x: 1.0,
+            y: 2.0,
+            z: 3.0,
+        };
+
+        let v = m * v;
+        let v_ref = Vec3{ x: 14.0, y: 32.0, z:50.0};
+
+        assert_eq!(v, v_ref);
     }
 }
